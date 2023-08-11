782d389aba1b360318c4a167f73f029cd8d591bc
Cristian <cristian.jimenezsa@hotmail.com>
2023-08-11 11:20:13 -0500

    first commit

---
+++ src/api/healthcheck/healthcheck.controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+export const healthcheckHandler = (req: Request, res: Response) => {
+   res.status(200).json({ message: "Server ok" });
+};
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import ConfigExpress from "./config/express";
+import routes from "./routes";
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+ConfigExpress(app);
+routes(app);
+
+app.listen(port, () => {
+   console.log(`App listening on port ${port}`);
+});
