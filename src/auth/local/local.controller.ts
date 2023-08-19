f967c2bea833679bdfd75e38c6ed3c6c368655b6
jesus bravo <jd_bravo24@hotmail.com>
2023-08-19 00:41:24 -0500

    refactoring schema prisma

--- src/auth/local/local.controller.ts
+++ src/auth/local/local.controller.ts
@@ -8,32 +8,34 @@ export async function loginHandler(req: Request, res: Response){
   const { email, password } = req.body;
 
   try {
-    const users = await getUserByEmail(email);
+    const user = await getUserByEmail(email);
 
-    if(!users) {
+    if(!user) {
       return res.status(401).send('Invalid credentials');
     }
 
-    const isMatch = await comparePassword(password, users.password)
+    const isMatch = await comparePassword(password, user.password)
     
     if(!isMatch) {
       return res.status(401).send('Invalid credentials');
     }
 
     const payload = {
-      id: users.id,
-      email: users.email,
+      id: user.id,
+      email: user.email,
     }
     const token = signToken(payload)
 
     const newUser = {
-      firstName: users.firstname,
-      lastName: users.lastname,
-      email: users.email,      
-      role: users.roleId
+      firstName: user.firstname,
+      lastName: user.lastname,
+      email: user.email,      
+      role: user.roleId
     }
 
     return res.status(200).json({ token, newUser})
 
-  } catch(error) {}
+  } catch (error: any) {
+    res.status(400).send({ message: error.message });
+  }
 }
