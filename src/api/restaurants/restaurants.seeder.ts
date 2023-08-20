6f9ede9ddbe63692d4a9015d6522f007b62bb343
Cristian <cristian.jimenezsa@hotmail.com>
2023-08-20 02:04:25 -0500

    test data removed from .seeder files

--- src/api/restaurants/restaurants.seeder.ts
+++ src/api/restaurants/restaurants.seeder.ts
@@ -1,46 +1,4 @@
 import { RestaurantsSeeder } from './restaurants.types';
 import { fakerRestaurants } from '../../faker/faker';
 
-export const restaurantsSeeder: RestaurantsSeeder[] = [
-  {
-    id: 'cllim4uqk0001wrwk5xsa9ff0',
-    nit: '900203',
-    businessName: 'Restro',
-    title: 'Italian Restro',
-    phone: '4321',
-    address: ['salt lake 32', 'evergreen 33'],
-    about: 'best pizza',
-    location: ['75', '30'],
-  },
-  {
-    id: 'cllim4uqk0002wrwk5xsa9ff0',
-    nit: '900489',
-    businessName: 'Chinese',
-    title: 'Asian Fusion',
-    phone: '8456',
-    address: ['chinatown 43', 'green 56'],
-    about: 'best sushi',
-    location: ['87', '56'],
-  },
-  {
-    id: 'cllim4uqk0003wrwk5xsa9ff0',
-    nit: '900547',
-    businessName: 'Mexican',
-    title: 'Orale Taco',
-    phone: '7754',
-    address: ['paletto bay 13', 'downtown 33'],
-    about: 'best taco',
-    location: ['65', '48'],
-  },
-  {
-    id: 'cllim4uqk0004wrwk5xsa9ff0',
-    nit: '900547',
-    businessName: 'Greek',
-    title: 'Restplace',
-    phone: '5568',
-    address: ['main road 45', 'vinehills 89'],
-    about: 'best mediterranean food',
-    location: ['53', '02'],
-  },
-  ...fakerRestaurants,
-];
+export const restaurantsSeeder: RestaurantsSeeder[] = [...fakerRestaurants];
