7262e1fba49c254a3b04c9e32e67ed2780ec916a
Cristian <cristian.jimenezsa@hotmail.com>
2023-08-28 14:00:22 -0500

    back to original restaurants route name

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { Application } from 'express';
-import restaurantsRouter from './api/restaurants/index.ts';
 import healthcheckRouter from './api/healthcheck/index.ts';
+import restaurantsRouter from './api/restaurants/index.ts';
 import facilitiesRouter from './api/facilities/index.ts';
 import reviewsRouter from './api/reviews/index.ts';
 import servicesRouter from './api/services/index.ts';
@@ -11,8 +11,8 @@ import productsRouter from './api/products/index.ts';
 import rolesRouter from './api/roles/index.ts';
 
 const routes = (app: Application) => {
-  app.use('/restaurants', restaurantsRouter);
   app.use('/api/healthcheck', healthcheckRouter);
+  app.use('/api/restaurants', restaurantsRouter);
   app.use('/api/facilities', facilitiesRouter);
   app.use('/api/reviews', reviewsRouter);
   app.use('/api/services', servicesRouter);
