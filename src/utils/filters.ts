281efbb70c732f9f4d8f4a80611c918d17686fb0
Cristian <cristian.jimenezsa@hotmail.com>
2023-09-06 00:36:31 -0500

    commented code removed

--- src/utils/filters.ts
+++ src/utils/filters.ts
@@ -1,5 +1,4 @@
 import { RestaurantsFiltered } from '../api/restaurants/restaurants.types';
-import { PaginationResult } from '../pagination/pagination.types';
 
 export const filteredData = (data: RestaurantsFiltered[], filter: string) => {
   if (filter == 'all') {
@@ -36,89 +35,22 @@ export const filteredByObject = (
   star: string,
   delivery: string,
 ) => {
-  console.log(cuisine, star, delivery);
-
   let result = [];
-
-  if (cuisine && !star && !delivery) {
-    const filteredByCuisine = data.filter(
-      (item: RestaurantsFiltered) => item.cuisines.includes(cuisine) && item,
-    );
-    result = filteredByCuisine;
-    return result;
-  }
-
-  if (!cuisine && star && !delivery) {
-    result = data.filter(
-      (item: RestaurantsFiltered) => item.rating >= parseInt(star) && item,
-    );
-    console.log('solo  star');
-    // result = filteredByStar;
-    return result;
-  }
-
-  if (!cuisine && !star && delivery) {
-    const filteredByDelivery = data.filter(
-      (item: RestaurantsFiltered) =>
-        item.delivery_time <= parseInt(delivery) && item,
-    );
-    result = filteredByDelivery;
-    return result;
-  }
 
   const filteredByCuisine = data.filter(
     (item: RestaurantsFiltered) => item.cuisines.includes(cuisine) && item,
   );
-  // console.log('cuisinesFiltered', filteredByCuisine.length);
   const filteredByDelivery = filteredByCuisine.filter(
     (subItem: RestaurantsFiltered) =>
       subItem.delivery_time <= parseInt(delivery) && subItem,
   );
-
-  // console.log('deliveryFiltered', filteredByDelivery.length);
 
   const filteredByStar = filteredByDelivery.filter(
     (lastItem: RestaurantsFiltered) =>
       lastItem.rating >= parseInt(star) && lastItem,
   );
 
-  // console.log('starFiltered', filteredByStar.length);
-
   result = filteredByStar;
-  // console.log(result);
 
   return result;
-};
-
-export const generator = (
-  data: RestaurantsFiltered[],
-  page: number,
-  limit: number,
-) => {
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-
-  const results = {} as PaginationResult;
-
-  // if (data) {
-  if (endIndex < data.length) {
-    results.next = {
-      page: page + 1,
-      limit: limit,
-    };
-  }
-
-  if (startIndex) {
-    results.previous = {
-      page: page - 1,
-      limit: limit,
-    };
-  }
-
-  results.length = data.length;
-
-  results.data = data.slice(startIndex, endIndex);
-
-  return results;
-  // }
 };
