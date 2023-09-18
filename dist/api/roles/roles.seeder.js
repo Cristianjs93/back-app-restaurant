35bf972a4c2c53ee4d8ad8b32324dbaf7e52fad0
Cristian <cristian.jimenezsa@hotmail.com>
2023-09-17 20:54:15 -0500

    roles reorder

--- dist/api/roles/roles.seeder.js
+++ dist/api/roles/roles.seeder.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.rolesSeeder = void 0;
 exports.rolesSeeder = [
     { id: 'cllim4uqr0001wrwkmd6zb8um', name: 'ADMIN' },
-    { id: 'cllim4uqr0003wrwk6fm5ym43', name: 'CLIENT' },
-    { id: 'cllim4uqr0002wrwknljozf6r', name: 'RESTAURANT' },
+    { id: 'cllim4uqr0002wrwk6fm5ym43', name: 'CLIENT' },
+    { id: 'cllim4uqr0003wrwknljozf6r', name: 'RESTAURANT' },
 ];
